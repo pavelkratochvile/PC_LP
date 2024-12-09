1cc303f63fdc343e697624ac9fd3ac2abe62a681
Pavel Kratochvíle <pavel.kratochvile@gmail.com>
2024-12-09 20:05:20 +0100

    Doplnění Big M metody

---
+++ Semestrální_práce/build/CMakeFiles/parser.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/pavel/Documents/PC/Semestrální_práce/src/main.c" "CMakeFiles/parser.dir/src/main.c.o" "gcc" "CMakeFiles/parser.dir/src/main.c.o.d"
+  "/mnt/c/Users/pavel/Documents/PC/Semestrální_práce/src/parse.c" "CMakeFiles/parser.dir/src/parse.c.o" "gcc" "CMakeFiles/parser.dir/src/parse.c.o.d"
+  "/mnt/c/Users/pavel/Documents/PC/Semestrální_práce/src/simplex.c" "CMakeFiles/parser.dir/src/simplex.c.o" "gcc" "CMakeFiles/parser.dir/src/simplex.c.o.d"
+  "/mnt/c/Users/pavel/Documents/PC/Semestrální_práce/src/table/table.c" "CMakeFiles/parser.dir/src/table/table.c.o" "gcc" "CMakeFiles/parser.dir/src/table/table.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Semestrální_práce/build/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/src/main.c.o"
+  "CMakeFiles/parser.dir/src/main.c.o.d"
+  "CMakeFiles/parser.dir/src/parse.c.o"
+  "CMakeFiles/parser.dir/src/parse.c.o.d"
+  "CMakeFiles/parser.dir/src/simplex.c.o"
+  "CMakeFiles/parser.dir/src/simplex.c.o.d"
+  "CMakeFiles/parser.dir/src/table/table.c.o"
+  "CMakeFiles/parser.dir/src/table/table.c.o.d"
+  "parser"
+  "parser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
